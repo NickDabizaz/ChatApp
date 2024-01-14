db89ec28ae8aa35e6af3aca0a08a67bb6e969e34
NickDabizaz <94162428+NickDabizaz@users.noreply.github.com>
2024-01-14 22:35:45 +0700

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -62,8 +62,15 @@ const router = createBrowserRouter([
 //   },
 // ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router}></RouterProvider>
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
