f1bcfb14958ecf7988df1a9063ee80d7d2c570da
Revereign <tenata4321@gmail.com>
2024-01-15 10:08:28 +0700

    change local bntr

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ const socket = require("socket.io");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: "https://main--dapper-mousse-59f527.netlify.app", // Mengizinkan akses dari alamat ini
+  // origin: "https://main--dapper-mousse-59f527.netlify.app", // Mengizinkan akses dari alamat ini
+  origin: "http://localhost:5173",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
@@ -44,7 +45,8 @@ const server = app.listen(PORT, async () => {
 
 const io = socket(server, {
   cors: {
-    origin: "https://main--dapper-mousse-59f527.netlify.app",
+    // origin: "https://main--dapper-mousse-59f527.netlify.app",
+    origin: "http://localhost:5173",
     credentials: true,
   },
 });
